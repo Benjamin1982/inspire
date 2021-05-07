6bc6d4b1376f5c272cce9b96e64fe40ce95b0b01
Benjamin <benjaminz.1@gmx.de>
2021-05-07 16:15:33 +0300

    Created App manually

---
+++ src/JavaUebungen.java
@@ -0,0 +1,20 @@
+public class JavaUebungen {
+    // ----------------------------------------//#endregion
+    private static String scandal = "SCANDAL";
+
+    // ---------------------------------------- ALT GR + 8 /9
+    public static void main(String[] args)
+
+    {
+        System.out.println("definitely my comupter");
+        WhileLoopBook();
+    }
+
+    static void WhileLoopBook()
+
+    {
+        int roaming = 0;
+        roaming = roaming + 1;
+        System.out.println("definitely my comupter");
+    }
+}
